d3378be9ebd2b5ddd8507ae7ea480feadff27643
Wojciech Jaszczak <wojciech.jaszczak@contractors.roche.com>
2017-03-22 15:07:19 +0100

    Subscriptions using helper method from library.

--- simple-subscriptions/client/src/modules/patients/sagas/subscribePatients.ts
+++ simple-subscriptions/client/src/modules/patients/sagas/subscribePatients.ts
@@ -2,10 +2,13 @@ import { takeLatest, take, call, race, fork, put, cancel} from 'redux-saga/effec
 import { delay, eventChannel } from 'redux-saga';
 import { actionsIDs } from '../constants'
 import { fetchPatients } from './fetchPatients';
+import { createStartHandler } from 'redux-saga-subscriptions';
 
 export const POOLING_TIMEOUT_MS = 5000; //in ms
 
 const createChannel = (payload?) => eventChannel((emit) => {
+	Promise.resolve(fetchPatients()).then(emit);
+
 	const interval = setInterval(() =>
 		Promise.resolve(fetchPatients()).then(emit),
 		POOLING_TIMEOUT_MS
@@ -14,27 +17,7 @@ const createChannel = (payload?) => eventChannel((emit) => {
 	return () => {clearInterval(interval)}; //Cleanup
 });
 
-export const startSubscription = function *() {
-	yield call(fetchPatients);
-	const channel = createChannel();
-
-	while (true) {
-		const {action, cancel} = yield race({
-			action: take(channel),
-			cancel: take(actionsIDs.STOP_SUBSCRIBE),
-		});
-
-		if (action) {
-			yield action;
-		}
-
-		if (cancel) {
-			channel.close();
-			return;
-		}
-	}
-};
-
 export const watchPatientsSubscription = function *() {
-  yield takeLatest(actionsIDs.START_SUBSCRIBE, startSubscription);
+	const startHandler = createStartHandler([actionsIDs.STOP_SUBSCRIBE]);
+	yield takeLatest(actionsIDs.START_SUBSCRIBE, startHandler(createChannel));
 };
