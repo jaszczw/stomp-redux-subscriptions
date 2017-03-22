9061175ab4b6b5c49da4e76970cfa720974db7b4
Wojciech Jaszczak <wojciech.jaszczak@contractors.roche.com>
2017-03-22 16:15:15 +0100

    Fixes references, whitespace.

--- simple-subscriptions/client/src/modules/patients/sagas/subscribePatients.ts
+++ simple-subscriptions/client/src/modules/patients/sagas/subscribePatients.ts
@@ -7,18 +7,18 @@ import { createStartHandler } from 'redux-saga-subscriptions';
 export const POOLING_TIMEOUT_MS = 5000; //in ms
 
 const createChannel = (payload?) => eventChannel((emit) => {
-	Promise.resolve(fetchPatients()).then(emit);
+  Promise.resolve(fetchPatients()).then(emit);
 
-	const interval = setInterval(() =>
-		Promise.resolve(fetchPatients()).then(emit),
-		POOLING_TIMEOUT_MS
-	);
+  const interval = setInterval(() =>
+      Promise.resolve(fetchPatients()).then(emit),
+    POOLING_TIMEOUT_MS
+  );
 
-	return () => {clearInterval(interval)}; //Cleanup
+  return () => {clearInterval(interval)}; //Cleanup
 });
 
 
 export const watchPatientsSubscription = function *() {
-	const startHandler = createStartHandler([actionsIDs.STOP_SUBSCRIBE]);
-	yield takeLatest(actionsIDs.START_SUBSCRIBE, startHandler(createChannel));
+  const startHandler = createStartHandler([actionsIDs.STOP_SUBSCRIBE]);
+  yield takeLatest(actionsIDs.START_SUBSCRIBE, startHandler(createChannel));
 };
